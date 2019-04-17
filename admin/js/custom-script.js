bd2d9009cad801e6d17523685bcd31bbcee04432
priyankabehera <priyanka.behera@multidots.com>
2019-04-17 12:12:20 +0530

    remove all files

--- admin/js/custom-script.js
+++
@@ -1,5 +0,0 @@
-(function($) {
-    $(window).load(function() {
-        
-});
-})(jQuery);
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// If this file is called directly, abort.
-if ( !defined( 'ABSPATH' ) ) {
-    exit;
-}
-// Silence is golden
